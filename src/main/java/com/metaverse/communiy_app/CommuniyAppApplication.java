d3d7db17a45380dddd1b1c558db408150cdd0fe6
citeFred <citefred@gmail.com>
2025-07-18 15:28:34 +0900

    chore: First Init

---
+++ src/main/java/com/metaverse/communiy_app/CommuniyAppApplication.java
@@ -0,0 +1,13 @@
+package com.metaverse.communiy_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuniyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommuniyAppApplication.class, args);
+    }
+
+}
