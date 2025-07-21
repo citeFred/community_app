a14aa2d6b7b3ee37c4fc1d7ba3ad68f1e1ab3a27
citeFred <citefred@gmail.com>
2025-07-22 08:22:01 +0900

    refactor: 특정 게시글 조회 시, 게시판이름, 관련댓글, 관련파일을 모두 조회(지연로딩) 할 수 있도록 ResponseDto 수정

--- src/main/java/com/metaverse/community_app/article/dto/ArticleResponseDto.java
+++ src/main/java/com/metaverse/community_app/article/dto/ArticleResponseDto.java
@@ -2,22 +2,30 @@ package com.metaverse.community_app.article.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.metaverse.community_app.article.domain.Article;
-import lombok.AllArgsConstructor;
+import com.metaverse.community_app.comment.dto.CommentResponseDto; // 댓글 DTO 임포트
+import com.metaverse.community_app.file.dto.FileResponseDto; // 파일 DTO 임포트
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class ArticleResponseDto {
     private Long id;
     private String title;
     private String content;
     private String boardTitle;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createAt;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime modifiedAt;
+
+    private List<CommentResponseDto> comments;
+    private List<FileResponseDto> files;
 
     public ArticleResponseDto(Article article) {
         this.id = article.getId();
@@ -25,6 +33,23 @@ public class ArticleResponseDto {
         this.content = article.getContent();
         this.createAt = article.getCreatedAt();
         this.modifiedAt = article.getModifiedAt();
-        this.boardTitle = article.getBoard().getTitle(); // 연관관계 정보 가져오기(Lazy Loading) 실행부
+
+        this.boardTitle = article.getBoard().getTitle();
+
+        if (article.getComments() != null) {
+            this.comments = article.getComments().stream()
+                    .map(CommentResponseDto::new)
+                    .collect(Collectors.toList());
+        } else {
+            this.comments = List.of();
+        }
+
+        if (article.getFiles() != null) {
+            this.files = article.getFiles().stream()
+                    .map(FileResponseDto::new)
+                    .collect(Collectors.toList());
+        } else {
+            this.files = List.of();
+        }
     }
 }
