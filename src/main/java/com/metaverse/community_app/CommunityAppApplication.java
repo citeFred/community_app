86489c7e0472e12d730d5f4966dfef167ed61105
citeFred <citefred@gmail.com>
2025-07-23 07:37:28 +0900

    refactor: Serializing PageImpl 관련 경고, 간결한 페이징 처리 응답을 위한 추가 부분
    - 핵심 페이징 정보만 간결하게 반환
    - PageImpl의 내부 구현이 변경되더라도 VIA_DTO 모드는 표준화된 "page" 객체만 노출하므로, JSON 응답 구조의 안정성을 보장, 클라이언트 측의 서버 응답 의존성 낮춤

--- src/main/java/com/metaverse/community_app/CommunityAppApplication.java
+++ src/main/java/com/metaverse/community_app/CommunityAppApplication.java
@@ -3,13 +3,13 @@ package com.metaverse.community_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @EnableJpaAuditing
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @SpringBootApplication
 public class CommunityAppApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CommunityAppApplication.class, args);
     }
-
 }
