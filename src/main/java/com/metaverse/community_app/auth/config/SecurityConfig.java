6db045384aeea1aa2db4b5de81b647c22c27d1ec
citefred <citefred@gmail.com>
2025-07-31 11:44:20 +0900

    fix: 프론트엔드 리액트 요청 CORS 포트 설정 추가

--- src/main/java/com/metaverse/community_app/auth/config/SecurityConfig.java
+++ src/main/java/com/metaverse/community_app/auth/config/SecurityConfig.java
@@ -84,7 +84,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500", "http://localhost:5500", "null"));
+        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:3000", "http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:3000","null"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
